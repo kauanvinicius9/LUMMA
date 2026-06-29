779ba6bb6f1b09083cc77197a10d1246d3a0a899
kauanvinicius9 <kauan.francischini@aluno.senai.br>
2026-06-29 18:31:14 -0300

    configure PostgreSQL and connect Spring-Boot

--- backend/src/main/java/com/lumma/backend/entity/User.java
+++ backend/src/main/java/com/lumma/backend/entity/User.java
@@ -7,19 +7,19 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name = "users", schema = "lumma")
+@Table(name="users", schema="lumma")
 public class User extends BaseEntity {
 
-    @Column(nullable = false)
+    @Column(nullable=false)
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable=false, unique=true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable=false)
     private String password;
 
-    @Column(name = "avatar_url")
+    @Column(name="avatar_url")
     private String avatarUrl;
 
     private String status;
